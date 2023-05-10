f3ae83698e2216eb6c9f6a2fa5e47544c1b9e63b
KimJooHyun <hg116701@naver.com>
2023-05-10 21:51:10 +0900

    1강완료

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,2 +1,65 @@
-package com.cos.security1.config;public class SecurityConfig {
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
+public class SecurityConfig {
+
+    // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다?
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_MANAGER")
+                .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행시켜줌
+                .defaultSuccessUrl("/");     // 로그인 성공시 돌아가는 줒소
+
+        return http.build();
+    }
 }
+
+
+
+
+
+/*
+    WebSecurityConfigurerAdapter를 상속하고 configure를 상속하는 방식
+    WebSecurityConfigurerAdapter가 deprecated 되어 다른 방법 사용
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+            .antMatchers("/user/**").authenticated() // /user/** 로 들어오는 url은 인증이 필요함
+            .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+            .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+            .anyRequest().permitAll() // user, manager, admin url이 아니면 다 허용
+            .and()
+            .formLogin()
+            .loginPage("/login"); // 로그인 페이지 설정
+    }
+}
+*/
--- src/main/java/com/cos/security1/repository/UserRepository.java
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package com.cos.security1.repository;public interface UserRepository {
+package com.cos.security1.repository;
+
+import com.cos.security1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// CRUD 함수를 JpaRepository가 들고 있음
+// @Repository라는 @Annotation이 없어도 IoC 됨
+// JpaRepostiory를 상속받았기 때문
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    // findBy 규칙 -> Username 문법
+    // select * from user where username = ?
+    public User findByUsername(String username);
+
 }
